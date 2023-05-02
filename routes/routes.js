d08dc570d35ed6a65114a6bdf147f9a5977f8b04
Mats Gundersen <matsgundersen@hotmail.com>
2023-05-02 13:33:00 +0200

    test

--- routes/routes.js
+++ routes/routes.js
@@ -98,7 +98,15 @@ async (req, res) => {
     const { htmlData, type } = req.body;
     if (type === 'pdf') {
         try {
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({
+                args: [
+                  '--no-sandbox',
+                  '--disable-setuid-sandbox',
+                  '--disable-dev-shm-usage',
+                  '--single-process',
+                ],
+              });
+              
             const page = await browser.newPage();
             await page.setContent(htmlData);
       
